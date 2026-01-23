232d274efe8a0aaa7b452ef1312102eacad3df1f
hiroaki kitamura <kitamura@start-rec.net>
2026-01-23 20:02:53 +0900

    swiper

--- src/assets-recipe-basic/js/index.ts
+++ src/assets-recipe-basic/js/index.ts
@@ -1,9 +1,23 @@
 import '@css/index.scss'
+import 'swiper/css/bundle'
+import Swiper from 'swiper'
+import { isPC } from '@/utils'
 
 document.addEventListener('DOMContentLoaded', () => {
   init()
 })
 
 function init() {
-  //
+  initSwiper()
+}
+
+function initSwiper() {
+  if (isPC()) {
+    return
+  }
+
+  new Swiper('#js-related-articles-swiper', {
+    slidesPerView: 'auto' as 'auto',
+    speed: 500,
+  })
 }
